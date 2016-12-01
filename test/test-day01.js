d76da386550d2d17c8dd383f1a7a75b2d1e2eddf
Przemek <mrnz@wp.pl>
2016-12-01 11:49:53 +0000

    env

---
+++ test/test-day01.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+
+
+describe('Day 1', function() {
+
+  describe('Part One', function() {
+
+    it('should return 0 when input string is empty', function () {
+      assert.equal( 0, 0 );
+    });
+
+  });
+
+  describe('Part Two', function() {
+    
+    it('should return false when input string is empty', function () {
+      assert.equal( false, false );
+    });
+
+  });
+
+});
