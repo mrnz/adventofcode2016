1e429f7f282fbe782e8e1d88872e120e9e385074
mrnz <mrnz@wp.pl>
2016-12-08 16:06:24 +0100

    day 8

--- day08/8-1.js
+++ day08/8-1.js
@@ -1,9 +1,45 @@
 'use strict';
 module.exports = ( commands, sizeX, sizeY ) => {
   
-  var matrix = Array(sizeX).fill( Array(sizeY).fill(0) );
+  var rect,
+      matrix,
+      rotateCol,
+      rotateRow;
+
+  matrix = Array(sizeX).fill( Array(sizeY).fill(0) );
   
-  
+  rect = ( X, Y ) => {
+    var calcFunc = ( x, idx ) => idx < parseInt(X) ? 1 : x;
+    for( let i = 0; i < parseInt(Y); i++ ){
+      matrix[i] = matrix[ i ].map( calcFunc );
+    }
+  };
+
+  rotateCol = ( column, shift ) => {
+    
+    var temp = [];
+    
+    matrix.forEach( ( row,idx ) => temp.push( row[ column ] ) );
+    
+    temp = temp.join('');
+    temp = temp.substring( temp.length-shift ) + temp.substring( 0, temp.length-shift );
+    
+    matrix = matrix.map( (row,idx) => {
+      row[column] = parseInt(temp[idx]);
+      return row;
+    });
+
+  };
+
+  rotateRow = ( row, shift ) => {
+    
+    var temp = matrix[ row ].join( '' );
+
+    temp = temp.substring( temp.length-shift ) + temp.substring( 0, temp.length-shift );
+       
+    matrix[ row ] = temp.split('').map( x => parseInt( x ) );
+
+  };  
 
   commands.split('\n').forEach( line => {
     
@@ -12,11 +48,7 @@ module.exports = ( commands, sizeX, sizeY ) => {
     if( line[0] === 'rect' ) {
 
       line = line[1].split('x');
-      let X = parseInt( line[0] );
-      let Y = parseInt( line[1] );
-      for( let i = 0; i < Y; i++ ){
-        matrix[i] = matrix[ i ].map( ( x, idx ) => idx < X ? 1 : x );
-      };
+      rect( line[0], line[1] );
 
     }else if (line[0] === 'rotate'){
         
@@ -24,28 +56,13 @@ module.exports = ( commands, sizeX, sizeY ) => {
         
         let column = parseInt( line[2].split('=')[1] );
         let shift = parseInt( line[4] );
-        let temp = [];
-        
-        matrix.forEach( (row,idx) => temp.push( row[column] ) );
-        
-        temp = temp.join('');
-
-        temp = temp.substring( temp.length-shift ) + temp.substring( 0, temp.length-shift );
-        
-        matrix = matrix.map( (row,idx) => {
-          row[column] = parseInt(temp[idx]);
-          return row;
-        });
+        rotateCol( column, shift );
 
       }else if(line[1] === 'row' ){
 
         let row = parseInt( line[2].split('=')[1] );
         let shift = parseInt( line[4] );
-        let temp = matrix[row].join('');
-
-        temp = temp.substring( temp.length-shift ) + temp.substring( 0, temp.length-shift );
-           
-        matrix[row] = temp.split('').map(x=>parseInt(x));
+        rotateRow( row, shift );
    
       }
 
--- test/test-day08.js
+++ test/test-day08.js
@@ -6,7 +6,7 @@ describe('Day 8', function() {
   describe('Part One', function() {
 
     it('should return 6 when input is an example from advent of code', function () {
-      assert.equal( 6, day8part1( 'rect 3x2\nrotate column x=1 by 1\nrotate row y=0 by 4\nrotate column x=1 by 1',3,7) );0
+      assert.equal( 6, day8part1( 'rect 3x2\nrotate column x=1 by 1\nrotate row y=0 by 4\nrotate column x=1 by 1',3,7) );
     });
 
   });
