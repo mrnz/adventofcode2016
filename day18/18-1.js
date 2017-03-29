91d6e58f37c034b1b01c798a5d51f3283a712a94
Przemek <mrnz@Przemeks-MacBook-Air.local>
2017-03-29 21:53:28 +0200

    day 18 scaffold

---
+++ day18/18-1.js
@@ -0,0 +1,6 @@
+'use strict';
+module.exports = {
+    init(input) {
+        console.log(input);
+    }
+};
---
+++ test/test-day18.js
@@ -0,0 +1,23 @@
+var assert = require('assert'),
+    day18 = require('../day18/18-1.js');
+
+describe('Day 18', function() {
+
+  describe('Part One', function() {
+
+    // it('init function should return DDRRRD when input is ihgpwlah', function () {
+    //     assert.equal( 'DDRRRD', day18.init( 'ihgpwlah' ) );
+    // });
+
+
+  });
+
+  describe('Part Two', function() {
+
+    // it('init function should return 370 when input is ihgpwlah and part 2 param is true', function () {
+    //     assert.equal( 370, day18.init( 'ihgpwlah', true ) );
+    // });
+
+  });
+
+});
