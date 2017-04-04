a72d4f6edf831f7943dd37f98b1460ccd70abdfa
Przemek Marciniak <przemek.marciniak@racebets.com>
2017-04-04 15:47:09 +0200

    day 21 both parts

--- day21/day21.js
+++ day21/day21.js
@@ -1,3 +1,4 @@
 var input = require('fs').readFileSync(__dirname+'/day21-input.txt').toString().split('\n');
-console.log( `day 21 part 1 - result is: ${require('./21.js').init(input)}` );
-// console.log( `day 21 part 1 - result is: ${require('./21.js').init(input, 400000)}` );
+var input2 = require('fs').readFileSync(__dirname+'/day21-input.txt').toString().split('\n');
+console.log( `day 21 part 1 - result is: ${require('./21.js').init(input, 'abcdefgh')}` );
+console.log( `day 21 part 2 - result is: ${require('./21.js').init2(input2, 'fbgdceah')}` );
