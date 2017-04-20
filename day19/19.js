27240c93b569fcfec138c5db8157a4265a94d4f2
Przemek Marciniak <przemek.marciniak@racebets.com>
2017-04-20 13:31:08 +0200

    day19 partially

--- day19/19.js
+++ day19/19.js
@@ -3,65 +3,28 @@
 module.exports = {
 
     init(i) {
-        return i - ( ( 2 << Math.log2( i ) ) - i ) + 1;
+        // return i - ( ( 2 << Math.log2( i ) ) - i ) + 1;
+        // even faster solution:
+        // move first "1" to the end of the number in binary notation
+        return parseInt( (i.toString(2) + '1').substring(1),2 )
     },
 
     init2(input) {
 
-        var i = 0,
-            skipped = [],
-            stateArray = Array(8).fill(true).map((x,idx)=>[idx,idx]);
+        function largest3n2() {
 
-
-
-
-        function oposit(point, l) {
-
-            var half = l%2 ? Math.floor(l/2) : l / 2,
-                t = point + half;
-
-            // if(l%2){
-            //     half = Math.floor(l / 2 );
-            // }else{
-            //     half = l / 2 ;
-            // }
-
-            if(t>l-1){
-                t = t - l;
-            }
-
-            return t;
-
+            var s = 2;
+            while (s < input) s = s * 3 - 2;
+            return (s + 2) / 3;
         }
 
-        while (stateArray.length > 1) {
+        var winner = input - largest3n2() + 1;
 
-            let next = oposit(i, stateArray.length);
-            if(!(i%1)){
-                    console.log(i + ' ==> ' + next + ' ' + stateArray.length);
-            }
-
-            // console.log(stateArray.length);
-            if(stateArray.length === 2) {
-                // stateArray.splice(i-2,1);
-                // console.log(stateArray[0] + 1);
-                console.log(i);
-                console.log(stateArray);
-                stateArray = [];
-            } else {
-                stateArray.splice(next,1);
-                // stateArray = stateArray.filter((x,idx) => idx !== i);
-
-            }
-
-            i = (i > stateArray.length - 2) ? 0 : i+1;
-             //i++
-
-
+        if (winner >= largest3n2()) {
+          winner = winner * 2 - 9
         }
 
-        return stateArray.indexOf(true) + 1;
-
+        return winner;
     }
 
 };
--- day19/day19.js
+++ day19/day19.js
@@ -1,2 +1,10 @@
 // var input = require('fs').readFileSync(__dirname+'/day19-input.txt').toString();
-console.log( `day 19 part 1 - result is: ${require('./19.js').init(3001330)}` );
+console.log( `day 19 part 1 - result is: ${require('./19.js').init(5)}` );
+
+// console.log( `day 19 part 1 - result is: ${require('./19.js').init2(5)}` );
+// console.log( `day 19 part 1 - result is: ${require('./19.js').init2(6)}` );
+// console.log( `day 19 part 1 - result is: ${require('./19.js').init2(7)}` );
+// console.log( `day 19 part 1 - result is: ${require('./19.js').init2(8)}` );
+// console.log( `day 19 part 1 - result is: ${require('./19.js').init2(9)}` );
+// console.log( `day 19 part 1 - result is: ${require('./19.js').init2(10)}` );
+// console.log( `day 19 part 1 - result is: ${require('./19.js').init2(81)}` );
